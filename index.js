dfc9d76154f15da8557d6384a2da3ff704ec9bcb
takasimakakeru <kyunpangoogle@outlook.jp>
2026-07-14 05:16:38 +0000

    a

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// テスト用のURL
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Node.js backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
